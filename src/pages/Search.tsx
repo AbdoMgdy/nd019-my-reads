de0177494c7e215cdd80a9085301a55e4dd9f2a1
AbdoMgdy <abdoman46@hotmail.com>
2021-01-06 16:31:53 +0200

    check if book exists in store

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   Box,
   GridItem,
@@ -8,34 +8,44 @@ import {
   SimpleGrid,
 } from "@chakra-ui/react";
 import Shelf from "../components/Shelf";
-import { actions, ShelfType } from "../store/rootReducer";
-import { search } from "../BooksAPI";
+import { actions, BookType, ShelfType } from "../store/rootReducer";
+import { get, search } from "../BooksAPI";
 import { BookContext } from "../store/rootContext";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 const Search = () => {
-  const { dispatch } = useContext(BookContext);
+  const { state, dispatch } = useContext(BookContext);
+  const [books, setBooks] = useState(false);
   const getSearchResutls = (query: String) => {
-    if (query) {
-      search(query).then((res) => {
+    if (query !== "") {
+      search(query).then(async (res) => {
         console.log(res);
         try {
-          let books = res.map((book: any) => {
-            return {
-              id: book.id,
-              title: book.title,
-              authors: book.authors,
-              img: book.imageLinks.thumbnail,
-              shelf: ShelfType.none,
-            };
+          let books = res.map((b: any) => {
+            let currBook = state.bookList.find((cb) => cb.id === b.id);
+            if (currBook) {
+              return currBook;
+            } else {
+              return {
+                id: b.id,
+                title: b.title,
+                img: b.imageLinks.thumbnail,
+                authors: b.authors,
+                shelf: ShelfType.none,
+              };
+            }
           });
           dispatch(actions.setSearchResults(books));
+          setBooks(true);
         } catch {
           console.log("error Ocuured");
+          dispatch(actions.setSearchResults([]));
+          setBooks(false);
         }
       });
     } else {
       dispatch(actions.setSearchResults([]));
+      setBooks(false);
     }
   };
   return (
@@ -65,7 +75,11 @@ const Search = () => {
           />
         </GridItem>
       </SimpleGrid>
-      <Shelf title="Search Results" type={ShelfType.none}></Shelf>
+      {!books ? (
+        <p>No Results Found</p>
+      ) : (
+        <Shelf title="Search Results" type={ShelfType.none}></Shelf>
+      )}
     </Box>
   );
 };
