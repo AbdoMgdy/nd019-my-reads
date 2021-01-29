f70255530e5bf61c587089cf125cbd4428e66658
AbdoMgdy <abdoman46@hotmail.com>
2021-01-29 17:07:18 +0200

    switch question view bsed on auth user

--- would-you-rather/src/components/Question/index.tsx
+++ would-you-rather/src/components/Question/index.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { IUser } from "../../store/types";
 import PollCard from "../PollCard";
 
 const Question = () => {
+  const auth: IUser = useSelector((state: any) => state.auth);
   const { question_id } = useParams();
   const questions = useSelector((state: any) => state.questions);
   const question = questions[question_id];
+  const [mode, setMode] = useState("submit");
+  useEffect(() => {
+    if (question_id in auth.answers) setMode("result");
+    else setMode("submit");
+  }, [auth.answers, question_id]);
   return (
     <div>
-      <PollCard mode="answer" question={question} />
+      <PollCard mode={mode} question={question} />
     </div>
   );
 };
