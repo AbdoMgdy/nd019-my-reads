7d6ac47a29a0b3ae0510da83026846d8db016058
AbdoMgdy <abdoman46@hotmail.com>
2021-01-29 13:16:58 +0200

    move routes to their own file

---
+++ would-you-rather/src/routes.tsx
@@ -0,0 +1,40 @@
+import { Navigate } from "react-router-dom";
+import LeaderBoard from "./components/Leaderboard";
+import Login from "./components/login";
+import NewPollCard from "./components/NewPollCard";
+import Question from "./components/Question";
+import PollTabs from "./components/Tabs";
+const routes = (isLoggedIn: boolean) => [
+  {
+    path: "/app",
+    // element: isLoggedIn ? <PollTabs /> : <Navigate to="/login" />,
+    children: [
+      {
+        path: "/",
+        element: isLoggedIn ? <PollTabs /> : <Navigate to="/login" />,
+      },
+      {
+        path: "/new",
+        element: isLoggedIn ? <NewPollCard /> : <Navigate to="/login" />,
+      },
+      {
+        path: "/leaderboard",
+        element: isLoggedIn ? <LeaderBoard /> : <Navigate to="/login" />,
+      },
+      {
+        path: "questions/:question_id",
+        element: isLoggedIn ? <Question /> : <Navigate to="/login" />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: !isLoggedIn ? <Login /> : <Navigate to="/app" />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "/", element: <Navigate to="/login" /> },
+    ],
+  },
+];
+
+export default routes;
