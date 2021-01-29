e5747657eb2f35a1eb73708a9077598c3c0fcc73
AbdoMgdy <abdoman46@hotmail.com>
2021-01-29 13:17:52 +0200

    finish polltabs

--- would-you-rather/src/components/Tabs/index.tsx
+++ would-you-rather/src/components/Tabs/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-import { Tabs } from "antd";
+import { Tabs, Space } from "antd";
 import PollCard from "../PollCard";
-import NewPollCard from "../NewPollCard";
+import { useSelector } from "react-redux";
+import { IQuesions, IUser } from "../../store/types";
 
 const { TabPane } = Tabs;
 
@@ -20,13 +21,41 @@ const test = {
   },
 };
 function PollTabs() {
+  const questions: IQuesions = useSelector((state: any) => state.questions);
+  const auth: IUser = useSelector((state: any) => state.auth);
+  console.log(auth);
+  console.log(questions);
+  const filteredQuestions = () => {
+    let answeredQuestions = [];
+    let unAnsweredQuestions = [];
+    for (let q in questions) {
+      if (q in auth.answers) {
+        answeredQuestions.push(questions[q]);
+      } else {
+        unAnsweredQuestions.push(questions[q]);
+      }
+    }
+    return {
+      answeredQuestions,
+      unAnsweredQuestions,
+    };
+  };
+  const qList = filteredQuestions();
   return (
     <Tabs defaultActiveKey="1" type="card" size="large">
-      <TabPane tab="Answered" key="1">
-        <PollCard question={test} mode="view" />
+      <TabPane tab="Unanswered" key="1">
+        <Space direction="vertical">
+          {qList.unAnsweredQuestions.map((q) => (
+            <PollCard key={q.id} question={q} mode="view" />
+          ))}
+        </Space>
       </TabPane>
-      <TabPane tab="Unanswered" key="2">
-        <PollCard question={test} mode="result" />
+      <TabPane tab="Answered" key="2">
+        <Space direction="vertical">
+          {qList.answeredQuestions.map((q) => (
+            <PollCard key={q.id} question={q} mode="result" />
+          ))}
+        </Space>
       </TabPane>
     </Tabs>
   );
