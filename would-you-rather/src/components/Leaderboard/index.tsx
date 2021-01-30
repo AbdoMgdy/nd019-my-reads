d828c9586a1c14f0074e25a5990fce802a3b77de
AbdoMgdy <abdoman46@hotmail.com>
2021-01-30 14:55:46 +0200

    sort leaderboard based on scores

--- would-you-rather/src/components/Leaderboard/index.tsx
+++ would-you-rather/src/components/Leaderboard/index.tsx
@@ -6,6 +6,15 @@ import { IUsers } from "../../store/types";
 const LeaderBoard = () => {
   const users: IUsers = useSelector((state: any) => state.users);
   const usersList = Object.entries(users);
+  usersList.sort((a, b) => {
+    const answereda = Object.keys(a[1].answers).length;
+    const askeda = Object.keys(a[1].answers).length;
+    const scorea = answereda + askeda;
+    const answeredb = Object.keys(b[1].answers).length;
+    const askedb = Object.keys(b[1].answers).length;
+    const scoreb = answeredb + askedb;
+    return scoreb - scorea;
+  });
   return (
     <div>
       <Space direction="vertical">
