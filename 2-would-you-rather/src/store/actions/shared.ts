e8f473b828a31e424647256a041181120579b4bd
AbdoMgdy <abdoman46@hotmail.com>
2021-04-29 05:24:14 +0200

    refactor

--- 2-would-you-rather/src/store/actions/shared.ts
+++ 2-would-you-rather/src/store/actions/shared.ts
@@ -1,9 +1,9 @@
 import { getInitialData } from "../../utils/api";
 import { receiveQuestions } from "./questions";
 import { receiveUsers } from "./users";
-export function handleInitialData(dispatch: any) {
+export const handleInitialData = () => (dispatch: any) => {
   return getInitialData().then(({ users, questions }) => {
     dispatch(receiveQuestions(questions));
     dispatch(receiveUsers(users));
   });
-}
+};
